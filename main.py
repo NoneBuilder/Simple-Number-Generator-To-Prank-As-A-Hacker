02fce56538edc5120333ae305c0fa24c3ebd23bd
NoneBuilder <81272062+NoneBuilder@users.noreply.github.com>
2021-03-24 08:01:01 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+  i = 1
+  while i <= 30:#(You can change the value according to what you want, delete this message when you copypaste, this value will determine how long the code will be executed)
+    j = 1
+    while j <= i:
+      print("%d " % (i*j), end='')
+      j += 1
+     print()
+    i += 1
+main()
